3aa4913e241de8f041c2f380792357a97d8af976
Rogher Adriano Soares <163779899+Roogher@users.noreply.github.com>
2025-04-11 23:54:05 -0300

    fazendo a mensagem dentro do li e voltando a exibir 'tarefa adiconada'"

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 function adicionarTarefa() {
-    //let mensagem = "Tarefa adicionada com sucesso!";
+    let mensagem = "Tarefa adicionada com sucesso!";
+    document.getElementById("mensagem").textContent = mensagem;
 
     let inputTarefa = document.getElementById("inputTarefa");
     let tarefa = inputTarefa.value;
-    document.getElementById("mensagem").textContent = tarefa;
 
     let listaTarefas = document.getElementById("listaTarefas");
     let novaTarefa = document.createElement("li");
+    
+    novaTarefa.textContent = tarefa; // Adiciona a tarefa à lista
 
     listaTarefas.appendChild(novaTarefa);
 
