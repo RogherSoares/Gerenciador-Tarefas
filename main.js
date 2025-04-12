f9afe66b155066297981b7af687016078c5fc692
Rogher Adriano Soares <163779899+Roogher@users.noreply.github.com>
2025-04-11 23:32:43 -0300

    interatividade para o texto do input aparecer

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 function adicionarTarefa() {
-    let mensagem = "Tarefa adicionada com sucesso!";
-        document.getElementById("mensagem").textContent = mensagem;
+    //let mensagem = "Tarefa adicionada com sucesso!";
+
+    let inputTarefa = document.getElementById("inputTarefa");
+    let tarefa = inputTarefa.value;
+    document.getElementById("mensagem").textContent = tarefa;
 }
