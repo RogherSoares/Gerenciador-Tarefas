648a5da5e45ae09adbf83326f9ce722650998a24
Rogher Adriano Soares <163779899+Roogher@users.noreply.github.com>
2025-04-12 00:30:04 -0300

    deixando o codigo semanticamente  mais correto trocando algumas variaveis por constantes

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function adicionarTarefa() {
 
     // Obtém o valor do campo de entrada 
-    let inputTarefa = document.getElementById("inputTarefa");
+    const inputTarefa = document.getElementById("inputTarefa");
     let tarefa = inputTarefa.value.trim(); // Remove espaços em branco no início e no final 
 
-    let mensagem = document.getElementById("mensagem");
+    const mensagem = document.getElementById("mensagem");
 
     // Verifica se o campo de entrada está vazio
     if (tarefa == "") {
@@ -17,7 +17,7 @@ function adicionarTarefa() {
         mensagem.textContent = mensagemSucesso;
 
         // Cria um novo elemento de lista para a tarefa
-        let listaTarefas = document.getElementById("listaTarefas");
+        const listaTarefas = document.getElementById("listaTarefas");
         let novaTarefa = document.createElement("li");
 
         // Adiciona a tarefa à lista
