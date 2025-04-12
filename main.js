4b78e983e2717c1802a0045fc894ee91875a90e4
Rogher Adriano Soares <163779899+Roogher@users.noreply.github.com>
2025-04-11 23:34:58 -0300

    linpanmdo o campo de input depois de adicionar a tarefa

--- main.js
+++ main.js
@@ -4,4 +4,6 @@ function adicionarTarefa() {
     let inputTarefa = document.getElementById("inputTarefa");
     let tarefa = inputTarefa.value;
     document.getElementById("mensagem").textContent = tarefa;
+
+    inputTarefa.value = ""; // Limpa o campo de entrada após adicionar a tarefa
 }
