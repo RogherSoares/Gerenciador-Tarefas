bb41bc4efad4fe5c89f9ef8f4ee75e5e36d746d1
Rogher Adriano Soares <163779899+Roogher@users.noreply.github.com>
2025-04-12 00:10:54 -0300

    organiznado as mensgens de 'erro'e 'sucesso' dentro das condicionais 'if' 'else' "

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 function adicionarTarefa() {
-    // Exibe uma mensagem de sucesso ao adicionar a tarefa
-    let mensagemSucesso = "Tarefa adicionada com sucesso!";
-    document.getElementById("mensagem").textContent = mensagemSucesso;
 
     // Obtém o valor do campo de entrada 
     let inputTarefa = document.getElementById("inputTarefa");
     let tarefa = inputTarefa.value;
 
+    // Verifica se o campo de entrada está vazio
+    if (tarefa == "") {
+        // Exibe uma mensagem de erro se o campo estiver vazio
+        let mensagemErro = "Por favor, insira uma tarefa.";
+        document.getElementById("mensagem").textContent = mensagemErro;
+    } else {
+        // Exibe uma mensagem de sucesso ao adicionar a tarefa
+        let mensagemSucesso = "Tarefa adicionada com sucesso!";
+        document.getElementById("mensagem").textContent = mensagemSucesso;
+    }
+
     // Cria um novo elemento de lista para a tarefa
     let listaTarefas = document.getElementById("listaTarefas");
     let novaTarefa = document.createElement("li");
 
-    // Verifica se o campo de entrada está vazio
-    if(tarefa == "") {
-        // Exibe uma mensagem de erro se o campo estiver vazio
-        let mensagemErro = "Por favor, insira uma tarefa.";
-        document.getElementById("mensagem").textContent = mensagemErro;
-    }
-    
+
+
     // Adiciona a tarefa à lista
-    novaTarefa.textContent = tarefa; 
+    novaTarefa.textContent = tarefa;
 
     listaTarefas.appendChild(novaTarefa);
 
     // Limpa o campo de entrada após adicionar a tarefa
-    inputTarefa.value = ""; 
+    inputTarefa.value = "";
 }
